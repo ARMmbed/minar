b30d3e35c51e08517bc401d5148cdc16edf9be24
Liyou Zhou <liyou.zhou@arm.com>
2016-01-13 18:13:57 +0000

    Enable warnings normally, can be disabled via a yotta config

--- source/minar.cpp
+++ source/minar.cpp
@@ -29,9 +29,6 @@
 
 //#define __MINAR_TRACE_MEMORY__
 //#define __MINAR_TRACE_DISPATCH__
-//#ifdef NDEBUG
-#define __MINAR_NO_RUNTIME_WARNINGS__
-//#endif
 
 #ifdef __MINAR_TRACE_MEMORY__
 extern "C" {
@@ -51,7 +48,7 @@ extern "C" {
 #define ytTraceDispatch(...) do{}while(0)
 #endif
 
-#ifndef __MINAR_NO_RUNTIME_WARNINGS__
+#ifndef YOTTA_CFG_MINAR_NO_RUNTIME_WARNINGS
 #include <stdio.h>
 #define ytWarning(...) printf(__VA_ARGS__)
 #else
